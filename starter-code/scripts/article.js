407c548db8d62cd99347a0a9b1446aad2cafef3b
Gloria Guy <totallygloria@gmail.com>
2016-09-22 13:46:46 -0700

    Used handlebars to compile the article template from the html.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -22,6 +22,9 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
+  var source = $('#article-template').html();
+  var template = Handlebars.compile(source);
+
   // TODO: Use the function that Handlebars gave you to return your filled-in
   //       html template for THIS article.
 };
@@ -37,3 +40,4 @@ ourLocalData.forEach(function(ele) {
 articles.forEach(function(a){
   $('#articles').append(a.toHtml());
 });
+
